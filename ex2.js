9fc223111c30e8cf7c5ad851e4151678e8fd30b5
LeonarEQ <leonardestupinan@yahoo.com>
2022-01-05 02:33:37 +0100

    Ejercicio Resuelto

--- ex2.js
+++ ex2.js
@@ -1,4 +1,8 @@
 /**
- * Ejercicio 2: Escribe un bucle, parecido al del ejercicio 1, 
+ * Ejercicio 2: Escribe un bucle, parecido al del ejercicio 1,
  * que muestre por el terminal el mensaje "Yo también estoy en un bucle!" ; un total de 20 veces
  */
+
+for (let i = 0; i < 20; i++) {
+  console.log(1 + i + ".-" + " Yo también estoy en un bucle!");
+}
