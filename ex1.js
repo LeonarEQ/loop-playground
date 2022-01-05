c5958ba18487faebcd0998c6b0dc15c4c368dd6e
LeonarEQ <leonardestupinan@yahoo.com>
2022-01-05 02:32:08 +0100

    Ejercicio Resuelto

--- ex1.js
+++ ex1.js
@@ -2,6 +2,6 @@
  * Ejercicio 1: Escribe un bucle que muestre por consola 10 veces la palabra "Estoy en un bucle!" El ejercicio está casi bien, falta arreglar alguna cosa. Ejecútalo para ver que sucede.
  */
 
-for (let i=0; i<10; i++) {
-    console.log("Estoy en un bucle!")
+for (let i = 0; i < 10; i++) {
+  console.log(1 + i + ".-" + " Estoy en un bucle!");
 }
