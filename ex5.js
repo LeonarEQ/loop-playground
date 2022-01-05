f5ef9cbd86bae7bd7f6864e07d822149994da063
LeonarEQ <leonardestupinan@yahoo.com>
2022-01-05 02:51:41 +0100

    ejercicio resuelto

--- ex5.js
+++ ex5.js
@@ -1,15 +1,20 @@
 /**
- * Ejercicio 5: El siguiente bucle no se ejecuta nunca. 
+ * Ejercicio 5: El siguiente bucle no se ejecuta nunca.
  * Comrpruébalo con "node ex5.js"
  * ¿Sabrías decir porque?
- * 
+ *
  * ---------------------------------------------------
- * Una vez lo averigues, arreglalo para que el bucle se ejecute exactamente 10 veces. 
+ * Una vez lo averigues, arreglalo para que el bucle se ejecute exactamente 10 veces.
  * Reestricción: SOLO puedes cambiar el valor de la variable 'veces' para conseguirlo
  */
 
- let veces = 5;
+/*let veces = 5;
 
  for (let i=5; i<veces; i++) {
      console.log("No me voy a ejecutar...")
- }
+ }*/
+let veces = 10;
+
+for (let i = 0; i < veces; i++) {
+  console.log("Ahora si me ejecuté " + (1 + i) + " veces");
+}
